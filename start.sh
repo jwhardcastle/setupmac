9fc60c5800a38b58ee4167826110e1fa89f3b788
Jack Hardcastle <jwhardcastle@jwhardcastle.com>
2017-10-14 18:09:05 -0400

    New svnpush, .ssh/config, and some other small improvements

--- start.sh
+++ start.sh
@@ -5,12 +5,22 @@ echo "==========================================="
 echo "Setting up your machine using jwhardcastle/setupmachine"
 echo "==========================================="
 
+ANSIBLE_CONFIG=
+
 # Ubuntu needs a bunch of libs before installing Ansible
 sudo sh -c "which apt && apt -yf install python-setuptools python-dev libffi-dev libssl-dev"
 
 sudo easy_install pip
 sudo easy_install ansible
 
+# We might be running this locally from a version of the git repo, in that case,
+# just run it, no need to clone anything
+if [ -e playbook.yml ]; then
+    ansible-playbook -Ki ./hosts --ask-vault-pass playbook.yml --verbose
+    exit $?
+fi
+
+# Else, we're likely running from a cURL so get the repo and run it
 installdir="/tmp/setupmachine-$RANDOM"
 mkdir $installdir
 
